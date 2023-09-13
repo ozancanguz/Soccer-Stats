794285618802badf02ac7e3d624c16a9516efafb
ozancanguz <ozancanguz24@gmail.com>
2023-09-13 16:23:32 +0300

    navigation setup created

---
+++ app/src/main/java/com/example/soccer_stats/ui/fragments/DetailsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.soccer_stats.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.soccer_stats.R
+import com.example.soccer_stats.databinding.FragmentDetailsBinding
+import com.example.soccer_stats.databinding.FragmentListBinding
+
+class DetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Initialize the binding object
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        // Return the root view of the binding
+
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/example/soccer_stats/ui/fragments/ListFragment.kt
@@ -0,0 +1,27 @@
+package com.example.soccer_stats.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.soccer_stats.R
+import com.example.soccer_stats.databinding.FragmentListBinding
+
+
+class ListFragment : Fragment() {
+    private lateinit var binding: FragmentListBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Initialize the binding object
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        // Return the root view of the binding
+        return binding.root
+    }
+
+
+}
