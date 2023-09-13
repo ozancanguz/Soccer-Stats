c7d14ba397de18d28a86d3fa8e16868111a7f80d
ozancanguz <ozancanguz24@gmail.com>
2023-09-13 16:30:11 +0300

    data classes created from api.

---
+++ app/src/main/java/com/example/soccer_stats/data/Result.kt
@@ -0,0 +1,8 @@
+package com.example.soccer_stats.data
+
+data class Result(
+    val away: String,
+    val date: String,
+    val home: String,
+    val skor: String
+)
---
+++ app/src/main/java/com/example/soccer_stats/data/Results.kt
@@ -0,0 +1,6 @@
+package com.example.soccer_stats.data
+
+data class Results(
+    val result: List<Result>,
+    val success: Boolean
+)
